8fde806b336d9f9896742ad6b40d1a8909168be6
hoo <bhwkd@naver.com>
2022-11-13 12:20:36 +0900

    feat: Add email duplicate check

--- Bottles_BE/Bottles/app/views.py
+++ Bottles_BE/Bottles/app/views.py
@@ -22,23 +22,23 @@ def user_list(request):
         serializer = UsersSerializer(all_users, many=True)
         return JsonResponse(serializer.data, safe=False)
 
-#존재하는 id인지 확인
-def isExist(Model, target):
-    return Model.objects.filter(id=target).exists()
 
 #회원가입
 class SignupView(APIView):
     def post(self, request):
-        print(1)
         #아이디 중복 검사
-        if(isExist(Users, request.data['id'])):
+        if(Users.objects.filter(id=request.data['id']).exists()):
             return Response({ "error": "already exist id"},status=409)
+        #이메일 중복 검사
+        if(Users.objects.filter(email=request.data['email']).exists()):
+            return Response({ "error": "already exist email"},status=409)
+
+        #렝스검사
+        #https://hashcode.co.kr/questions/8710/%EC%95%88%EB%85%95%ED%95%98%EC%84%B8%EC%9A%94-%EC%9E%A5%EA%B3%A0-is_valid%EC%9D%98-%EB%8F%99%EC%9E%91%EB%B0%A9%EC%8B%9D%EC%9D%B4-%EA%B6%81%EA%B8%88%ED%95%A9%EB%8B%88%EB%8B%A4
+
         #회원정보저장
-        print(2)
         #data = JSONParser().parse(request)
-        print(3)
-        serializer = UsersSerializer_SignUp(data=request.data)
-        print(4)
+        serializer = UsersSerializer_SignUp(data=request.data)  
         if serializer.is_valid():
             serializer.save()
             return Response({"register successfully"},status=200)
